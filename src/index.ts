28a34fd20da1ff7778294584f82f1a8989e42d2b
GabrielGQA <gabrielgqa@gmail.com>
2023-04-16 21:46:26 -0300

    feat: project base

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
