9dc93cdac5cbefa5930116d1bbb7bdf72d752398
GabrielGQA <gabrielgqa@gmail.com>
2023-04-17 11:13:31 -0300

    feat: configura knex

---
+++ knexfile.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import { Knex } from 'knex';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: Knex.Config = {
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSOWRD,
+    database: process.env.DB_DATABASE,
+    port: 5432
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'config', 'db', 'migrations')
+  },
+};
+
+export default config;
