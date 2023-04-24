3f7e4eb61bdbba97c9af24406e4e8161646290fc
GabrielGQA <gabrielgqa@gmail.com>
2023-04-24 19:33:38 -0300

    feat: model category

--- src/repositories/category.repository.ts
+++ src/repositories/category.repository.ts
@@ -1,10 +1,12 @@
 import { Knex } from 'knex';
+import { Category } from '../entities/Category';
 
 export class CategoryRepository {
   constructor(private readonly knex: Knex) {}
 
   async create(name: string, description: string) {
-    return await this.knex('categories').insert({ name, description });
+    const category = await this.knex('categories').insert({ name, description });
+    return await this.formatEntitie(category);
   }
 
   async findAll() {
@@ -21,5 +23,14 @@ export class CategoryRepository {
 
   async delete(id: number) {
     return await this.knex('categories').where({ id }).del();
+  }
+
+  async formatEntitie(category: any[]): Promise<Category>  {
+    const entitieCategory = new  Category();
+    entitieCategory.id = category[0].id;
+    entitieCategory.name = category[0].name;
+    entitieCategory.description = category[0].description;
+
+    return entitieCategory;
   }
 }
